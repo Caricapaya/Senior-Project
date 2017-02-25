0ee0746e1d3e140b79719c9304941e24f1c0512f
lifesaverimpact <lifesaverimpact@gmail.com>
2017-02-25 13:34:27 -0600

    Before merge for code review

--- app/src/main/java/com/example/christopher/myapplication/Profile.java
+++ app/src/main/java/com/example/christopher/myapplication/Profile.java
@@ -44,17 +44,45 @@ public class Profile extends AppCompatActivity {
         // 지역 스피너에 데이터 출력
 
         // arrays.xml에 정의한 String 배열 ( age ) 를 ArrayAdapter로 바인딩하고 스피너의 모양을 지정
-        ArrayAdapter<CharSequence> adapterLocal = ArrayAdapter.createFromResource(this, R.array.age, android.R.layout.simple_spinner_item);
-
+        ArrayAdapter<CharSequence> adapterLocalAge = ArrayAdapter.createFromResource(this, R.array.age, android.R.layout.simple_spinner_item);
         // ArrayAdapter객체에 할당된 데이터들을 스피너가 클릭될때 보여줄 스피너 아이템의 출력형식을 지정함
-        adapterLocal.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
+        adapterLocalAge.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // xml에서 정의한 age 스피너 객체 얻어오기
         final Spinner spinnerAge = (Spinner) findViewById(R.id.spinner_age);
         // 스피너에 타이틀메시지 넣기
         //spinnerAge.setPrompt("Select your age!~!");
         // 스피너에 ArrayAdapter를 연결함
-        spinnerAge.setAdapter(adapterLocal);
+        spinnerAge.setAdapter(adapterLocalAge);
+
+
+        // arrays.xml에 정의한 String 배열 ( music ) 를 ArrayAdapter로 바인딩하고 스피너의 모양을 지정
+        ArrayAdapter<CharSequence> adapterLocalMusic = ArrayAdapter.createFromResource(this, R.array.music, android.R.layout.simple_spinner_item);
+        // ArrayAdapter객체에 할당된 데이터들을 스피너가 클릭될때 보여줄 스피너 아이템의 출력형식을 지정함
+        adapterLocalMusic.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // xml에서 정의한 music 스피너 객체 얻어오기
+        final Spinner spinnerMusic = (Spinner) findViewById(R.id.spinner_music);
+        // 스피너에 ArrayAdapter를 연결함
+        spinnerMusic.setAdapter(adapterLocalMusic);
+
+
+        // arrays.xml에 정의한 String 배열 ( sports ) 를 ArrayAdapter로 바인딩하고 스피너의 모양을 지정
+        ArrayAdapter<CharSequence> adapterLocalSports = ArrayAdapter.createFromResource(this, R.array.sports, android.R.layout.simple_spinner_item);
+        // ArrayAdapter객체에 할당된 데이터들을 스피너가 클릭될때 보여줄 스피너 아이템의 출력형식을 지정함
+        adapterLocalSports.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // xml에서 정의한 sports 스피너 객체 얻어오기
+        final Spinner spinnerSports = (Spinner) findViewById(R.id.spinner_sports);
+        // 스피너에 ArrayAdapter를 연결함
+        spinnerSports.setAdapter(adapterLocalSports);
+
+
+        // arrays.xml에 정의한 String 배열 ( movie ) 를 ArrayAdapter로 바인딩하고 스피너의 모양을 지정
+        ArrayAdapter<CharSequence> adapterLocalMovie = ArrayAdapter.createFromResource(this, R.array.movie, android.R.layout.simple_spinner_item);
+        // ArrayAdapter객체에 할당된 데이터들을 스피너가 클릭될때 보여줄 스피너 아이템의 출력형식을 지정함
+        adapterLocalMovie.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // xml에서 정의한 sports 스피너 객체 얻어오기
+        final Spinner spinnerMovie = (Spinner) findViewById(R.id.spinner_movie);
+        // 스피너에 ArrayAdapter를 연결함
+        spinnerMovie.setAdapter(adapterLocalMovie);
 
 
     }
@@ -71,6 +99,9 @@ public class Profile extends AppCompatActivity {
     }
 
     public void onClick_update (View v){
+
+        String updateMessage = "Profile Updated";
+        Toast.makeText(Profile.this,updateMessage,Toast.LENGTH_SHORT).show();
         //Intent intent_toLogin = new Intent(getApplicationContext(), Login.class);
         //startActivity(intent_toLogin);
     }
